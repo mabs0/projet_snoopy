ba2b40df1b9ae564c207394543662c84e5abe1e0
mabs0 <118767637+mabs0@users.noreply.github.com>
2023-10-27 15:42:09 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int menu(char choix) {
-    printf("Menu\n 1. Règles du jeu\n 2. QCM\n 3. Dessiner un carre\n 4. Dessiner un sablier\n 6. Quitter\n"); //affiche le menu
+    printf("Menu\n 1. Règles du jeu\n 2. Lancer un nouveau jeu à partir du niveau\n "
+           "3. Charger une partie\n 4. Mot de passe\n 5. Scores\n 6. Quitter"); //affiche le menu
     printf("Veuillez choisir une des options :\n"); //demande à l'utilisateur de choisir une option dans le menu
-    //scanf("%d", &choix); //scan le choix de l'utilisateur
     fflush(stdin);
     scanf("%c", &choix);
 }
