84509bf853bdb57cd83e627b1066e324dc5374c7
HugoMBKC <149174933+HugoMBKC@users.noreply.github.com>
2023-10-27 16:32:23 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -5,6 +5,6 @@ int menu () {
     printf("\n\n          ....................\n          ........Menu........\n          ....................\n          ..1.Regles du jeu...\n          ..2.Nouvelle partie.\n"
            "          ..3.Charger.........\n          ..4.Mot de passe....\n          ..5.Scores..........\n          ..6.Quitter.........\n          ....................\n"); //affiche le menu
     fflush(stdin);
-    scanf("          %c", &choix);
+    scanf("%c", &choix);
     return 0;
 }
