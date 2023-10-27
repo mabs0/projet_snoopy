02584b78d2732deae8181ea12e156e6fb8980b92
mabs0 <118767637+mabs0@users.noreply.github.com>
2023-10-27 18:04:23 +0200

    Update scores.c

--- scores.c
+++ scores.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
-int scores() {
+typedef struct {
     int Sniveau1; //initialise le score du niveau 1
     int Sniveau2; //initialise le score du niveau 2
     int Sniveau3; //initialise le score du niveau 3
-    int time_rest1; //initialise le temps restant pour le niveau 1
-    int time_rest2; //initialise le temps restant pour le niveau 2
-    int time_rest3; //initialise le temps restant pour le niveau 3
     int Stotal; //initialise le score total
+}Score; 
+Score current_score;
+int time_rest1; //initialise le temps restant pour le niveau 1
+int time_rest2; //initialise le temps restant pour le niveau 2
+int time_rest3; //initialise le temps restant pour le niveau 3
+//faire fonction scanf pour temps restants et la mettre la
+current_score.Sniveau1 = time_rest1 * 100;
+current_score.Sniveau2 = time_rest2 * 100;
+current_score.Sniveau3 = time_rest3 * 100;
 
-    //faire fonction scanf pour temps restants et la mettre la
-    Sniveau1 = time_rest1 * 100;
-    Sniveau2 = time_rest2 * 100;
-    Sniveau3 = time_rest3 * 100;
-
-    Stotal = Sniveau1 + Sniveau2 + Sniveau3;
-
-    printf("Le score total du joueur est : %d", Stotal);
-}
+Stotal = Sniveau1 + Sniveau2 + Sniveau3;
