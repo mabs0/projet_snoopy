dd3f27b81ed6723fc93eb0ee17d41390878724bf
HugoMBKC <149174933+HugoMBKC@users.noreply.github.com>
2023-10-27 17:43:23 +0200

    Update interface.c

--- interface.c
+++ interface.c
@@ -1,4 +1,4 @@
 #include <stdio.h>
-void erreur() {
+void interface_erreur() {
   printf("\n\n\n....................\n......ERREUR......\n....................\n......VEUILLEZ......\n....................\n......CHOISIR.......\n....................\n.....UNE OPTION.....\n....................\n.....DISPONIBLE.....\n....................\n\n\n");
 }
