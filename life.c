1abb40e339d35b3c66766b04c1a210667369d4dd
mabs0 <118767637+mabs0@users.noreply.github.com>
2023-10-28 14:46:17 +0200

    Create life.c

---
+++ life.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int life() {
+    int vies=3;
+
+    if (/*variable du temps*/>120) {
+        vies-=1;
+        //variable qui recommence le niveau
+    }
+    if (/*variable de la balle qui touche snoopy*/) {
+        vies-=1;
+    }
+}
