c7a35a80076a51c550a926b13b86eb06d2b60da4
mabs0 <118767637+mabs0@users.noreply.github.com>
2023-10-28 15:36:32 +0200

    Update game_over.c

--- game_over.c
+++ game_over.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include "functions.h"
 
-void interface_game_over() {
+char interface_game_over() {
     printf("\n\n\n....................\n........GAME........\n"
-           "....................\n........OVER........\n....................\n");
+           "....................\n........OVER........\n....................\n\n");
+}
+
+void game_over() {
+    if (life()==0) {
+        printf("%c",interface_game_over());
+        /*retour au menu*/
+    }
 }
