c388de3ca1b2787c659bc93276b0a1286727ac1f
mabs0 <118767637+mabs0@users.noreply.github.com>
2023-10-31 12:46:28 +0100

    Add files via upload

---
+++ victory.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int victoire() {
+    if (/*variables des 4 oiseaux récupérés*/) {
+        printf("\n....................\n....................\n......VICTOIRE......\n"
+               "....................\n....................\n");
+        printf("",/*variable niveau suivant*/);
+    }
+};
