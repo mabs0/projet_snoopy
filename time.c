1c000397314fbe1354558a39907ee0acd239e1ca
mabs0 <118767637+mabs0@users.noreply.github.com>
2023-10-31 13:55:52 +0100

    Update time.c

--- time.c
+++ time.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 
-int time() {
-    int t_start=0; //temps au début
-    int t_niv=120; //temps d'un niveau
+int main() {
+    int seconds=120;
+    while (seconds>=0) {
+        printf("Temps restant :%d",seconds);
+        fflush(stdout);
+        sleep(1);
+        seconds--;
+        system("cls");
+    }
+    printf("Fin du temps !");
+    return 0;
 }
-
-//je crois je fais de la merde la :(
-//ref life.c
