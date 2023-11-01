edd55e8bbb06c4319afea58a0c695afb6b39f034
HugoMBKC <149174933+HugoMBKC@users.noreply.github.com>
2023-11-01 18:12:59 +0100

    Update scores.c

--- scores.c
+++ scores.c
@@ -5,14 +5,14 @@ typedef struct {
     int Sniveau2; //initialise le score du niveau 2
     int Sniveau3; //initialise le score du niveau 3
     int Stotal; //initialise le score total
-}Score; 
+}Score;
 Score current_score;
 int time_rest1; //initialise le temps restant pour le niveau 1
 int time_rest2; //initialise le temps restant pour le niveau 2
 int time_rest3; //initialise le temps restant pour le niveau 3
 //faire fonction scanf pour temps restants et la mettre la
-current_score.Sniveau1 = time_rest1 * 100;
-current_score.Sniveau2 = time_rest2 * 100;
-current_score.Sniveau3 = time_rest3 * 100;
+int current_score.Sniveau1 = time_rest1 * 100;
+int current_score.Sniveau2 = time_rest2 * 100;
+int current_score.Sniveau3 = time_rest3 * 100;
 
-Stotal = Sniveau1 + Sniveau2 + Sniveau3;
+int Stotal = Sniveau1 + Sniveau2 + Sniveau3;
