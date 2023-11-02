1291afafc0d5fedae1d906b07936c47612b191d6
HugoMBKC <149174933+HugoMBKC@users.noreply.github.com>
2023-11-02 18:06:00 +0100

    Update game_over.c

--- game_over.c
+++ game_over.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
-#include "functions.h"
-
-char interface_game_over() {
-    printf("\n\n\n....................\n........GAME........\n"
-           "....................\n........OVER........\n....................\n\n");
-}
+#include "interface.h"
 
 void game_over() {
     if (life()==0) {
-        printf("%c",interface_game_over());
+        interface_game_over();
         /*retour au menu*/
     }
 }
