e05b5c568d96728ef22519fe896618c810ffac7a
Kvely <149174834+Kvely@users.noreply.github.com>
2023-11-09 15:41:03 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -2,31 +2,54 @@
 #include "functions.h"
 #include "interface.h"
 
-void menu() {
-    int f;
+int main() {
+
+    int f,g;
+
+
     do {
-        interface_menu();
+        printf("\n\n....................\n........Menu........\n....................\n..1.Regles du jeu...\n..2.Nouvelle partie.\n"
+               "..3.Charger.........\n..4.Mot de passe....\n..5.Scores..........\n..6.Quitter.........\n....................\n"); //affiche le menu
         fflush(stdin); // assure le scanf
         scanf("%d", &f);
 
         switch (f) {
-            case 1:
+            case 1: //Règles du jeu
 
                 break;
 
-            case 2:
+            case 2: //Lancer un nouveau Jeu à partir du niveau 1
                 break;
 
-            case 3:
+            case 3: //Charger une partie
                 break;
 
-            case 4:
+            case 4: //Mot de passe
+
+            switch (MDP()) {
+                    case 1:
+                        // ils faux lancer le niveau 1
+                        break;
+                    case 2:
+                        // il faut lancer le niveau 2
+                        break;
+                    case 3:
+                        // il faut lancer le niveau 3
+                        break;
+                    case 0:
+                        printf("mauvais mot de passe ! ");
+                        break;
+                default:
+                    printf("erreur");
+                    break;
+            }
+
                 break;
 
-            case 5:
+            case 5: //Scores
                 break;
 
-            case 6 :
+            case 6 : //Quitter
                 printf("A bientot ! ");
                 break;
 
@@ -34,4 +57,6 @@ void menu() {
                 printf("veuillez choisir une des options donnée :");
         }
     }while (f != 6);
+
+    return 0;
 }
