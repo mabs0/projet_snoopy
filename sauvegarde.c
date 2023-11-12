8dd9dc98fee26f5f831d6d7dde750b7771d35a4e
mabs0 <118767637+mabs0@users.noreply.github.com>
2023-11-12 23:17:35 +0100

    Update sauvegarde.c

--- sauvegarde.c
+++ sauvegarde.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <windows.h>
 #include "scores.c"
-#include "interface.h"
+#include "functions.h"
 #include "structure_et_constante.c"
 Niveau sauvegarde1;
 Niveau sauvegarde2;
