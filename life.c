baf9ac94b90471db8efe40ed269657cc20b9eb97
mabs0 <118767637+mabs0@users.noreply.github.com>
2023-11-27 23:34:31 +0100

    Update life.c

--- life.c
+++ life.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include "functions.h"
+#include <unistd.h>
 
 int life() {
     int vies=3;
 
-    if (time()>120) {
-        vies-=1;
-        //variable qui recommence le niveau
+    while (vies>=0) {
+        printf("Points de vie restants : %d", vies);
+        fflush(stdout);
+        if (time() > 120) {
+            vies -= 1;
+            //variable qui recommence le niveau
+        }
+        if (variable de la balle qui touche snoopy) {
+            vies -= 1;
+        }
+        if (variable des ennemis qui touchent snoopy) {
+            vies -= 1;
+
+        }
     }
-    if (/*variable de la balle qui touche snoopy*/) {
-        vies-=1;
-    }
+    printf("Plus de vies, dommage !");
+
+    return vies;
 }
