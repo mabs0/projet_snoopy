ae16faa968fe54f4dd744c877070be3ba2c0644e
Kvely <149174834+Kvely@users.noreply.github.com>
2023-11-27 19:14:28 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
-#include "functions.h"
+#include "tkt.h"
+#include<windows.h>
 
 int main() {
 
     int f,g;
+    int niv[10][20];
+
+    printf("=================================================================================================\n");
+    printf("|    .____             __________                                 .__                           |\n");
+    printf("|    |    |   _____    \\______   \\ _______  ______    ____   ____ |  |__   ____                 |\n");
+    printf("|    |    |   \\__  \\    |       _// __ \\  \\/ |__  \\  /    \\_/ ___\\|  |  \\_/ __ \\                |\n");
+    printf("|    |    |___ / __ \\_  |    |   \\  ___/\\   / / __ \\|   |  \\  \\___|   Y  \\  ___/                |\n");
+    printf("|    |_______ (____  /  |____|_  /\\___  >\\_/ (____  /___|  /\\___  >___|  /\\___  >               |\n");
+    printf("|            \\/    \\/          \\/     \\/          \\/     \\/     \\/     \\/     \\/                |\n");
+    printf("|        .___         _________                                                                 |\n");
+    printf("|      __| _/____    /   _____/ ____   ____   ____ ______ ___.__.                               |\n");
+    printf("|     / __ |/ __ \\   \\_____  \\ /    \\ /  _ \\ /  _ \\\\____ <   |  |                               |\n");
+    printf("|    / /_/ \\  ___/   /        \\   |  (  <_> |  <_> )  |_> >___  |                               |\n");
+    printf("|    \\____ |\\___  > /_______  /___|  /\\____/ \\____/|   __// ____|                               |\n");
+    printf("|         \\/    \\/          \\/     \\/              |__|   \\/                                    |\n");
+    printf("=================================================================================================\n");
+    Sleep(5000);
 
 
     do {
@@ -18,6 +36,9 @@ int main() {
                 break;
 
             case 2: //Lancer un nouveau Jeu à partir du niveau 1
+                niveau(niv);
+                dghb();
+                mooveS(niv);
                 break;
 
             case 3: //Charger une partie
@@ -25,7 +46,7 @@ int main() {
 
             case 4: //Mot de passe
 
-            switch (MDP()) {
+                switch (MDP()) {
                     case 1:
                         // ils faux lancer le niveau 1
                         break;
@@ -38,10 +59,10 @@ int main() {
                     case 0:
                         printf("mauvais mot de passe ! ");
                         break;
-                default:
-                    printf("erreur");
-                    break;
-            }
+                    default:
+                        printf("erreur");
+                        break;
+                }
 
                 break;
 
@@ -59,3 +80,4 @@ int main() {
 
     return 0;
 }
+
