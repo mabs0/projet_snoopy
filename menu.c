78e6da7e44a0fde43aad25fbc6d9b15ad88918c3
HugoMBKC <149174933+HugoMBKC@users.noreply.github.com>
2023-11-30 17:51:48 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,47 +1,25 @@
-#include <stdio.h>
-#include "tkt.h"
-#include<windows.h>
+#include "functions.h"
 
-int main() {
-
+int menu() {
+    interface_titre();
     int f,g;
-    int niv[10][20];
-
-    printf("=================================================================================================\n");
-    printf("|    .____             __________                                 .__                           |\n");
-    printf("|    |    |   _____    \\______   \\ _______  ______    ____   ____ |  |__   ____                 |\n");
-    printf("|    |    |   \\__  \\    |       _// __ \\  \\/ |__  \\  /    \\_/ ___\\|  |  \\_/ __ \\                |\n");
-    printf("|    |    |___ / __ \\_  |    |   \\  ___/\\   / / __ \\|   |  \\  \\___|   Y  \\  ___/                |\n");
-    printf("|    |_______ (____  /  |____|_  /\\___  >\\_/ (____  /___|  /\\___  >___|  /\\___  >               |\n");
-    printf("|            \\/    \\/          \\/     \\/          \\/     \\/     \\/     \\/     \\/                |\n");
-    printf("|        .___         _________                                                                 |\n");
-    printf("|      __| _/____    /   _____/ ____   ____   ____ ______ ___.__.                               |\n");
-    printf("|     / __ |/ __ \\   \\_____  \\ /    \\ /  _ \\ /  _ \\\\____ <   |  |                               |\n");
-    printf("|    / /_/ \\  ___/   /        \\   |  (  <_> |  <_> )  |_> >___  |                               |\n");
-    printf("|    \\____ |\\___  > /_______  /___|  /\\____/ \\____/|   __// ____|                               |\n");
-    printf("|         \\/    \\/          \\/     \\/              |__|   \\/                                    |\n");
-    printf("=================================================================================================\n");
-    Sleep(5000);
-
-
     do {
-        printf("\n\n....................\n........Menu........\n....................\n..1.Regles du jeu...\n..2.Nouvelle partie.\n"
-               "..3.Charger.........\n..4.Mot de passe....\n..5.Scores..........\n..6.Quitter.........\n....................\n"); //affiche le menu
+        interface_menu();
         fflush(stdin); // assure le scanf
         scanf("%d", &f);
 
         switch (f) {
             case 1: //Règles du jeu
-
+                interface_regles_du_jeu();
                 break;
 
             case 2: //Lancer un nouveau Jeu à partir du niveau 1
-                niveau(niv);
-                dghb();
-                mooveS(niv);
+                clear_interface();
+                niveau(structure_niv_1, 9, 8, 5, 9);
                 break;
 
             case 3: //Charger une partie
+                interface_sauvegarde();
                 break;
 
             case 4: //Mot de passe
@@ -80,4 +58,3 @@ int main() {
 
     return 0;
 }
-
