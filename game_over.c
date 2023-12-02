85c1fbcb89f72bc93c81c2578eeb02924f52b280
Kvely <149174834+Kvely@users.noreply.github.com>
2023-12-02 17:50:04 +0100

    Update game_over.c

--- game_over.c
+++ game_over.c
@@ -1,7 +1,7 @@
-#include "functions.h"
+#include "functions.h" // Inclut le fichier "functions.h" au programme
 
 void game_over() {
-    if (life()==0) {
-        interface_game_over();
+    if (life() == 0) { // Vérifie si la fonction life() retourne 0
+        interface_game_over(); // Appelle la fonction d'interface pour afficher l'écran de fin de partie
     }
 }
