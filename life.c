55f0629b836fd8d2006318ca5381b9dd4c112de8
Kvely <149174834+Kvely@users.noreply.github.com>
2023-12-02 17:56:57 +0100

    Update life.c

--- life.c
+++ life.c
@@ -1,24 +1,25 @@
-#include "functions.h"
+#include "functions.h" // Inclusion du fichier "functions.h"
 
 int life() {
-    int vies=3;
+    int vies = 3; // Initialise le nombre de vies à 3
 
-    while (vies>=0) {
-        printf("Points de vie restants : %d", vies);
-        fflush(stdout);
-        //if (time() > 120) {
-          //  vies -= 1;
-            //variable qui recommence le niveau
-        //}
-        //if (/*variable de la balle qui touche snoopy*/) {
-        //    vies -= 1;
+    while (vies >= 0) { // Boucle tant que le nombre de vies est supérieur ou égal à zéro 
+        printf("Points de vie restants : %d", vies); // Affiche le nombre de vies restantes
+        fflush(stdout); // Vide le tampon de sortie pour afficher le texte immédiatement
+
+        // if (time() > 120) {
+        //     vies -= 1;
+        //     //variable qui recommence le niveau
+        // }
+        // if (/*variable de la balle qui touche snoopy*/) {
+        //     vies -= 1;
         // }
         // if (/*variable des ennemis qui touchent snoopy*/) {
         //     vies -= 1;
-
         // }
     }
-    printf("Plus de vies, dommage !");
 
-    return vies;
+    printf("Plus de vies, dommage !"); // Affiche 
+
+    return vies; // Retourne le nombre de vies restantes 
 }
