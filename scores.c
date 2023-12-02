34c208ed09fdf3d2f5252bf1df2454710e86ab88
Kvely <149174834+Kvely@users.noreply.github.com>
2023-12-02 18:14:31 +0100

    Update scores.c

--- scores.c
+++ scores.c
@@ -1,14 +1,12 @@
-#include "functions.h"
+#include "functions.h" // Inclut la fonction "functions.h"
 
-int score_general() {
+int score_general() { // Cette fonction ne retourne rien 
+    score score; // Déclare une variable de type score 
+    current_score current_score; // Déclare une variable de type current_score 
 
-    score score;
-    current_score current_score;
-
-    //faire fonction scanf pour temps restants et la mettre la
-    score.Sniveau1 = current_score.time_rest1 * 100;
-    score.Sniveau2 = current_score.time_rest2 * 100;
-    score.Sniveau3 = current_score.time_rest3 * 100;
-
-    score.Stotal = score.Sniveau1 + score.Sniveau2 + score.Sniveau3;
+    score.Sniveau1 = current_score.time_rest1 * 100; // Calcule le score pour le niveau 1 
+    score.Sniveau2 = current_score.time_rest2 * 100; // Calcule le score pour le niveau 2 
+    score.Sniveau3 = current_score.time_rest3 * 100; // Calcule le score pour le niveau 3 
+    score.Stotal = score.Sniveau1 + score.Sniveau2 + score.Sniveau3; // Calcule le score total en additionnant les scores des trois niveaux.
+    
 }
