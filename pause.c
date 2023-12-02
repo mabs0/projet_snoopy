115edace7f406d5bdf22d6030300c18827e3531d
Kvely <149174834+Kvely@users.noreply.github.com>
2023-12-02 18:11:37 +0100

    Update pause.c

--- pause.c
+++ pause.c
@@ -1,14 +1,13 @@
-#include "functions.h"
+#include "functions.h" // inclut la fonction "functions.h"
 
-int pause() {
-    if (_kbhit()) {
-        char key = _getch();
-        if (key == 'p' || key == 'P') {
-            // Mettre en pause
-            gotoligcol(13, 4);
-            printf("Jeu en pause");
-            while (_getch() != 'p' && _getch() != 'P') {} // Attendre jusqu'à ce que 'p' soit enfoncé à nouveau
-            printf("\033[2K");  // Efface la ligne actuelle
+int pause() { // Cette fonction ne renvoie rien 
+    if (_kbhit()) { // Vérifie s'il y a une frappe de touche dans la file d'attente du clavier.
+        char key = _getch(); // Si une touche est frappée, la fonction _getch() récupère cette touche et la stocke dans la variable 'key'.
+        if (key == 'p' || key == 'P') { // Si la touche frappée est 'p' ou 'P' , alors une pause du jeu est déclenchée.
+            gotoligcol(13, 4); // Appelle une fonction qui déplace le curseur à la ligne 13 et à la colonne 4 sur la console.  
+            printf("Jeu en pause"); // Affiche      
+            while (_getch() != 'p' && _getch() != 'P') {} // Attend jusqu'à ce que la touche 'p' ou 'P' soit à nouveau enfoncée pour reprendre le jeu.
+            printf("\033[2K"); // Efface la ligne actuelle dans la console.
         }
     }
 }
