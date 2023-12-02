d3ddfd4228d7a094d52d35f3510abca1ad0563ee
Kvely <149174834+Kvely@users.noreply.github.com>
2023-12-02 18:17:20 +0100

    Update time.c

--- time.c
+++ time.c
@@ -1,17 +1,17 @@
-#include "functions.h"
+#include "functions.h" // Inclut la fonction "functions.h"
 
 int horloge(){
-    clock_t temps;
-    temps = clock()/1000;
-    return temps;
+    clock_t temps; // Déclare une variable temps de type clock_t 
+    temps = clock()/1000; // Récupère le temps actuel de l'horloge CPU 
+    return temps; // Renvoie le temps actuel en secondes.
 }
 
-int temps_restant(int temps_init) {
-    int temps = 120 - (horloge() - temps_init);
-    return temps;
+int temps_restant(int temps_init) { // prend un argument temps_init 
+    int temps = 120 - (horloge() - temps_init); // Calcule le temps restant 
+    return temps; // Renvoie le temps restant calculé.
 }
 
 void timer (int temps_init) {
-    gotoligcol(11,0);
-    printf("Temps restant : %d s.", temps_restant(temps_init));
+    gotoligcol(11,0); // Appelle une fonction qui déplace le curseur à la ligne 11 et à la colonne 0 sur la console.
+    printf("Temps restant : %d s.", temps_restant(temps_init)); // Affiche le temps restant en secondes en utilisant la fonction temps_restant()
 }
