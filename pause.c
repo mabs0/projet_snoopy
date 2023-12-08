74da868908126ef100e2e57e492e0299b2603151
HugoMBKC <149174933+HugoMBKC@users.noreply.github.com>
2023-12-08 16:58:14 +0100

    Update pause.c

--- pause.c
+++ pause.c
@@ -1,13 +1,17 @@
 #include "functions.h" // inclut la fonction "functions.h"
 
-int pause() { // Cette fonction ne renvoie rien 
-    if (_kbhit()) { // Vérifie s'il y a une frappe de touche dans la file d'attente du clavier.
-        char key = _getch(); // Si une touche est frappée, la fonction _getch() récupère cette touche et la stocke dans la variable 'key'.
-        if (key == 'p' || key == 'P') { // Si la touche frappée est 'p' ou 'P' , alors une pause du jeu est déclenchée.
-            gotoligcol(13, 4); // Appelle une fonction qui déplace le curseur à la ligne 13 et à la colonne 4 sur la console.  
-            printf("Jeu en pause"); // Affiche      
-            while (_getch() != 'p' && _getch() != 'P') {} // Attend jusqu'à ce que la touche 'p' ou 'P' soit à nouveau enfoncée pour reprendre le jeu.
+int pause(char key, int temps_init) { // Cette fonction ne renvoie rien
+    int t1, t2;
+        if (key == 'p') { // Si la touche frappée est 'p', alors une pause du jeu est déclenchée.
+            t1=horloge();
+            gotoligcol(13, 4); // Appelle une fonction qui déplace le curseur à la ligne 13 et à la colonne 4 sur la console.
+            printf("Jeu en pause"); // Affiche
+            while (_getch() != 'p') {} // Attend jusqu'à ce que la touche 'p' ou 'P' soit à nouveau enfoncée pour reprendre le jeu.
             printf("\033[2K"); // Efface la ligne actuelle dans la console.
+            t2=horloge();
+            return temps_init+(t2-t1);
         }
-    }
+        else {
+            return temps_init;
+        }
 }
