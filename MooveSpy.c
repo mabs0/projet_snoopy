bbb38e48d1569171a33947f941280fd2ee919e2f
HugoMBKC <149174933+HugoMBKC@users.noreply.github.com>
2023-12-08 16:49:21 +0100

    Update MooveSpy.c

--- MooveSpy.c
+++ MooveSpy.c
@@ -5,18 +5,16 @@ void gotoligcol(int lig, int col) { // Définition de la structure COORD pour st
     // Assignation des valeurs de ligne et colonne aux coordonnées
     mycoord.X = col;
     mycoord.Y = lig;
-    
+
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), mycoord); // Déplace le curseur à l'emplacement spécifié en utilisant les coordonnées
 }
 
 int dghb() {
-    if (_kbhit()) { // Vérification 
+    if (_kbhit()) { // Vérification
         int depl = getch(); // Lecture d'un caractère du clavier
         gotoligcol(1, 1); // Déplace le curseur à la ligne 1, colonne 1
         return depl; // Renvoie la valeur dep1
     } else {
-        return 0; // Renvoie 0 
+        return 0; // Renvoie 0
     }
 }
-
-
