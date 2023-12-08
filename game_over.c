997839d62e71856a4d05189705b30d7b926431e2
HugoMBKC <149174933+HugoMBKC@users.noreply.github.com>
2023-12-08 16:52:35 +0100

    Update game_over.c

--- game_over.c
+++ game_over.c
@@ -1,7 +1,13 @@
-#include "functions.h" // Inclut le fichier "functions.h" au programme
+#include "functions.h"
 
-void game_over() {
-    if (life() == 0) { // Vérifie si la fonction life() retourne 0
+int game_over(int pv) {
+    if (pv<=0) {
+        clear_interface(); //Nettoie l'interface
         interface_game_over(); // Appelle la fonction d'interface pour afficher l'écran de fin de partie
+        sleep(3);
+        return 0; // Retuourne 0 à la variable var ce qui met fin au niveau
+    }
+    else {
+        return 1; // Retuourne 1 à la variable var ce qui qui fait continuer le niveau
     }
 }
