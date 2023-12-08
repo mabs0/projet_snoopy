59d9792cd200e4c40719c440a0afaff38ff5360d
HugoMBKC <149174933+HugoMBKC@users.noreply.github.com>
2023-12-08 16:53:17 +0100

    Update interface.c

--- interface.c
+++ interface.c
@@ -1,20 +1,19 @@
-#include "functions.h" // Inclut le fichier "functions.h" au programme
+#include "functions.h"
 
-void clear_interface(int x, int y) {
+void clear_interface(int x,int y) {
     int i, j;
-    // Boucle pour effacer l'interface
-    for (i = 0; i < LARGEUR; i++) { // Parcourt les lignes 
-        for (j = 0; j < LONGUEUR; j++) { // Parcourt les colonnes 
-            gotoligcol(i, j); // Déplace le curseur à la position (i, j)
-            printf(" "); // Affiche un espace pour effacer 
+    for (i=0;i<LARGEUR;i++) {
+        for (j=0;j<LONGUEUR;j++) {
+            gotoligcol(i,j);
+            printf(" ");
         }
-        printf("\n"); // Passe à la ligne suivante après avoir effacé
+        printf("\n");
     }
-    gotoligcol(y, x); // Replace le curseur à une position spécifique après avoir effacé l'interface
+    gotoligcol(y,x);
 }
 
-void interface_titre() { // Affiche
-    clear_interface(0,0); // clear l'interface 
+void interface_titre() {
+    clear_interface(0,0);
     printf("=================================================================================================\n");
     printf("|    .____             __________                                 .__                           |\n");
     printf("|    |    |   _____    \\______   \\ _______  ______    ____   ____ |  |__   ____                 |\n");
@@ -33,44 +32,45 @@ void interface_titre() { // Affiche
 }
 
 void interface_erreur() {
-    clear_interface(0,0); // clear l'interface 
+    clear_interface(0,0);
     printf("\n\n\n....................\n....................\n.......ERREUR.......\n......VEUILLEZ......\n......CHOISIR.......\n.....UNE OPTION.....\n.....DISPONIBLE.....\n....................\n....................\n....................\n\n\n"); //affiche l'écran d'erreur
 }
 
-void interface_sauvegarde() {
-    clear_interface(0,0); // clear l'interface 
-    printf("\n\n\n....................\n...Sauvegardes :....\n....................\n...1.Sauvegarde 1...\n....................\n...2.Sauvegarde 2...\n.....................\n...3.Sauvegarde 3...\n....................\n....................\n\n\n"); //affiche l'écran de sauvegarde
-}
 
 void interface_game_over() {
-    clear_interface(0,0); // clear l'interface 
+    clear_interface(0,0);
     printf("\n\n\n....................\n....................\n....................\n....................\n........GAME........\n........OVER........\n....................\n....................\n....................\n....................\n\n\n"); //affiche l'écran de GAME OVER
 }
 
 void interface_menu() {
-    clear_interface(0,0); // clear l'interface 
+    clear_interface(0,0);
     printf("\n\n\n....................\n........Menu........\n....................\n..1.Regles du jeu...\n..2.Nouvelle partie.\n..3.Charger.........\n..4.Mot de passe....\n..5.Scores..........\n..6.Quitter.........\n....................\n\n\n"); //affiche le menu
 }
 
 void interface_regles_du_jeu() {
-    clear_interface(0,0); // clear l'interface 
-    printf("\n\n\n....................\n....................\n.Le but de Snoopy...\n.est de recuperer...\n.4 oiseaux aux 4....\n.coins du niveau....\n.en un temps........\n..imparti...........\n....................\n....................\n\n\n"); // Affiche
-    sleep(4); // Attend 4 secondes 
-    clear_interface(0,0); // clear l'interface 
-    printf("\n\n\n....................\n....................\n.Pour cela, Snoopy..\n.va devoir utiliser.\n.tous les moyens....\n.a sa disposition...\n.et esquiver des....\n.obstacles..........\n....................\n....................\n\n\n"); // Affiche
-    sleep(4); // Attend 4 secondes 
-    clear_interface(0,0); // clear l'interface 
-    printf("\n\n\n....................\n....................\n.Snoopy peut........\n.deplacer et casser.\n.des blocs pour.....\n.atteindre les......\n.oiseaux a..........\n.recuperer..........\n....................\n....................\n\n\n"); // Affiche
-    sleep(4); // Attend 4 secondes 
-    clear_interface(0,0); // clear l'interface 
-    printf("\n\n\n....................\n....................\n.Mais il doit faire.\n.attention a la.....\n.balle et aux blocs.\n.pieges qui ont.....\n.pour but de........\n.l'arreter..........\n....................\n....................\n\n\n"); // Affiche
-    sleep(4); // Attend 4 secondes 
-    clear_interface(0,0); // clear l'interface 
-    printf("\n\n\n....................\n....................\n....................\n....................\n.......BONNE........\n.......CHANCE.......\n....................\n....................\n....................\n....................\n\n\n"); // Affiche
+    clear_interface(0,0);
+    printf("\n\n\n....................\n....................\n.Le but de Snoopy...\n.est de recuperer...\n.4 oiseaux aux 4....\n.coins du niveau....\n.en un temps........\n..imparti...........\n....................\n....................\n\n\n");
+    sleep(4);
+    clear_interface(0,0);
+    printf("\n\n\n....................\n....................\n.Pour cela, Snoopy..\n.va devoir utiliser.\n.tous les moyens....\n.a sa disposition...\n.et esquiver des....\n.obstacles..........\n....................\n....................\n\n\n");
+    sleep(4);
+    clear_interface(0,0);
+    printf("\n\n\n....................\n....................\n.Snoopy peut........\n.deplacer et casser.\n.des blocs pour.....\n.atteindre les......\n.oiseaux a..........\n.recuperer..........\n....................\n....................\n\n\n");
+    sleep(4);
+    clear_interface(0,0);
+    printf("\n\n\n....................\n....................\n.Mais il doit faire.\n.attention a la.....\n.balle et aux blocs.\n.pieges qui ont.....\n.pour but de........\n.l'arreter..........\n....................\n....................\n\n\n");
+    sleep(4);
+    clear_interface(0,0);
+    printf("\n\n\n....................\n....................\n....................\n....................\n.......BONNE........\n.......CHANCE.......\n....................\n....................\n....................\n....................\n\n\n");
+}
+
+void interface_touches() {
+    gotoligcol(14,0);
+    printf("Les 0 representent des murs infranchisables, le S Snoopy, le X la balle, les 6 des blocs deplacables, les 9 des blocs casssables intacts et les 3 des abimes\nDeplacez-vous avec les fleches, sautez avec espace pour abîmer les blocs cassables aux alentours, mettez en pause avec p, sauvegardez avec s et quittez avec q\nOh est dernier conseil, mefiez vous des 2...");
 }
 
 void interface_victoire() {
-    clear_interface(0,0); // clear l'interface 
-    printf("\n....................\n....................\n......VICTOIRE......\n" // Affiche
-           "....................\n....................\n");
+    clear_interface(0,0);
+    printf("\n....................\n....................\n....................\n....................\n......VICTOIRE......\n....................\n....................\n....................\n....................\n....................\n");
+    sleep(3);
 }
