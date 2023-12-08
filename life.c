e2ada1d6113aeed44e40db6718f3e99a8ca3c545
HugoMBKC <149174933+HugoMBKC@users.noreply.github.com>
2023-12-08 16:53:54 +0100

    Update life.c

--- life.c
+++ life.c
@@ -1,25 +1,6 @@
-#include "functions.h" // Inclusion du fichier "functions.h"
+#include "functions.h"
 
-int life() {
-    int vies = 3; // Initialise le nombre de vies à 3
-
-    while (vies >= 0) { // Boucle tant que le nombre de vies est supérieur ou égal à zéro 
-        printf("Points de vie restants : %d", vies); // Affiche le nombre de vies restantes
-        fflush(stdout); // Vide le tampon de sortie pour afficher le texte immédiatement
-
-        // if (time() > 120) {
-        //     vies -= 1;
-        //     //variable qui recommence le niveau
-        // }
-        // if (/*variable de la balle qui touche snoopy*/) {
-        //     vies -= 1;
-        // }
-        // if (/*variable des ennemis qui touchent snoopy*/) {
-        //     vies -= 1;
-        // }
-    }
-
-    printf("Plus de vies, dommage !"); // Affiche 
-
-    return vies; // Retourne le nombre de vies restantes 
+void life(int pv) {
+    gotoligcol(12,0);
+    printf("Points de vie restants : %d", pv);
 }
